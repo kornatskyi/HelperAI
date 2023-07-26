7e5af283570aa77c372ef9d783c28bf3657e4e7b
Bohdan Kornatskyi <bohdan.kornatskyi@gmail.com>
2023-07-25 20:04:33 -0700

    load environment variables from .env file

--- hai/__main__.py
+++ hai/__main__.py
@@ -1,10 +1,19 @@
+# standard
+import os
+from pathlib import Path
+# external
+from dotenv import load_dotenv
+
+# internal
 from hai.model.api_manager import ApiManager
 from hai.view.cli_view import CliView
 from hai.controller.ai_controller import AiController
 
+dotenv_path = Path('.env.development')
+load_dotenv(dotenv_path=dotenv_path)
 def main():
     api_model_name = "gpt-3.5-turbo-0613"
-    api_key = ""
+    api_key = os.getenv('OPENAI_API_KEY')
     api_manager = ApiManager(api_model_name, api_key)
     cli_view = CliView()
     
