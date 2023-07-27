05b92520921000a2548fefdb0e901c3ccf73ceae
Bohdan Kornatskyi <bohdan.kornatskyi@gmail.com>
2023-07-26 22:09:09 -0700

    basic copying capabilities

--- hai/controller/ai_controller.py
+++ hai/controller/ai_controller.py
@@ -1,6 +1,9 @@
+import pyperclip as pc
+
 from hai.model.api_manager import ApiManager
 from hai.view.cli_view import CliView
 
+  
 
 class AiController:
     def __init__(self, api_manager: ApiManager, cli_view: CliView):
@@ -8,10 +11,12 @@ class AiController:
         self.cli_view = cli_view
 
     def start(self):
-        while True:
-            user_input = self.cli_view.get_user_input()
-            if user_input.lower() == "quit":
-                break
-            response = self.api_manager.get_ai_response(user_input)
-            self.cli_view.show_output(response['content'])  # Assuming the response is already formatted
-            break
+        user_input = self.cli_view.get_user_input()
+        if user_input.lower() == "quit":
+            return  
+        response = self.api_manager.get_ai_response(user_input)
+        content_to_copy = self.cli_view.show_output(
+            response["content"]
+        )  # Assuming the response is already formatted
+        user_input = self.cli_view.get_user_input()
+        pc.copy(content_to_copy[int(user_input) - 1])
--- hai/view/cli_view.py
+++ hai/view/cli_view.py
@@ -9,6 +9,12 @@ class CliView:
         return input("> ")
 
     @staticmethod
-    def show_output(message):
+    def show_output(message) -> list[str]:
         markdown = Markdown(message)
+        contents: list[str] = []
+        for token in markdown.parsed:
+            if token.tag == "code":
+                contents.append(token.content)
+                token.content = token.content + f"(press {len(contents)} to copy)"
         CliView.console.print(markdown)
+        return contents
